9996b9316e6721c2a11b201fcc539de31d399e57
juankbeta <juan2.betancur@ucp.edu.co>
2024-09-10 00:46:36 -0400

    Build CarModel and CarMake Django Models

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,18 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarMake, CarModel
 
 
-# Register your models here.
+# Registering models with their respective admins
+class CarModelAdmin(admin.ModelAdmin):
+    fields = ['car_make', 'name', 'type', 'year', 'engine']
 
-# CarModelInline class
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 5  # Número de registros en blanco adicionales para agregar nuevos modelos directamente
 
-# CarModelAdmin class
+class CarMakeAdmin(admin.ModelAdmin):
+    fields = ['name', 'description', 'country_of_origin', 'founded_year', 'website']
+    inlines = [CarModelInline]
 
-# CarMakeAdmin class with CarModelInline
-
-# Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,25 +1,51 @@
 # Uncomment the following imports before adding the Model code
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
-
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    country_of_origin = models.CharField(max_length=100, blank=True, null=True)  # País de origen de la marca
+    founded_year = models.PositiveIntegerField(blank=True, null=True)  # Año de fundación de la marca
+    website = models.URLField(blank=True, null=True)  # Sitio web oficial de la marca
+    headquarters = models.CharField(max_length=255, blank=True, null=True)  # Sede principal de la marca
+
+    def __str__(self):
+        return self.name  # Return the name as the string representation
 
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many
-# Car Models, using ForeignKey field)
-# - Name
-# - Type (CharField with a choices argument to provide limited choices
-# such as Sedan, SUV, WAGON, etc.)
-# - Year (IntegerField) with min value 2015 and max value 2023
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+class CarModel(models.Model):
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Relación Many-to-One con CarMake
+    name = models.CharField(max_length=100)  # Nombre del modelo de carro
+    # Tipos de carro
+    CAR_TYPES = [
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+        ('COUPE', 'Coupe'),
+        ('CONVERTIBLE', 'Convertible'),
+        ('HATCHBACK', 'Hatchback'),
+        ('TRUCK', 'Truck'),
+        ('VAN', 'Van'),
+        ('SPORT', 'Sport'),
+        ('ELECTRIC', 'Electric'),
+    ]
+    type = models.CharField(max_length=12, choices=CAR_TYPES, default='SUV')  # Tipo de carro
+    year = models.IntegerField(default=2023,
+        validators=[
+            MaxValueValidator(2023),
+            MinValueValidator(2015)
+        ])
+    
+    # Atributos adicionales
+    engine = models.CharField(max_length=50, blank=True, null=True)  # Tipo de motor
+    transmission = models.CharField(max_length=20, choices=[('Automatic', 'Automática'), ('Manual', 'Manual')], blank=True, null=True)  # Tipo de transmisión
+    fuel_type = models.CharField(max_length=15, choices=[('Gasoline', 'Gasolina'), ('Diesel', 'Diésel'), ('Electric', 'Eléctrico'), ('Hybrid', 'Híbrido')], blank=True, null=True)  # Tipo de combustible
+    price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)  # Precio del modelo
+
+    def __str__(self):
+        return f"{self.car_make.name} {self.name} ({self.year})"
